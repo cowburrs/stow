a628986e59216c4f3ef10cac7748cffb2e4bd735
Your Name <you@example.com>
2026-04-20 21:36:40 +1000

    gitshit

--- burrs/.config/nvim/lua/plugins/coderunner.lua
+++ burrs/.config/nvim/lua/plugins/coderunner.lua
@@ -1,4 +1,4 @@
 return {
 	"CRAG666/code_runner.nvim",
-	config = { true, vim.keymap.set("n", "<f5>", "<cmd>RunCode<CR>i", { desc = "Run Code" }) },
+	config = { true, vim.keymap.set("n", "<F5>", "<cmd>RunCode<CR>i", { desc = "Run Code" }) },
 }
--- burrs/.config/nvim/lua/plugins/todo-comments.lua
+++ burrs/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,11 +1,10 @@
 -- Highlight todo, notes, etc in comments
 return {
-	{
+    -- enabled = false,
 		"folke/todo-comments.nvim",
 		event = "VimEnter",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = { signs = false },
-	},
 }
 -- PERF: ++ instead of +=.
 -- HACK: Code is optimized
